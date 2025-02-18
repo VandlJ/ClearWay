5fc2aefd521817f4fd5c9f568f368b5e65c9104e
Jan Vandlíček <jan.vandlicek@gmail.com>
2025-02-19 00:35:53 +0100

    Portal to mount the Popup properly

--- src/app/components/Popup.tsx
+++ src/app/components/Popup.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Portal from './Portal';
 
 interface PopupProps {
   title: string;
@@ -8,7 +9,8 @@ interface PopupProps {
 
 export default function Popup({ title, onClose, options }: PopupProps) {
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-gray-400 bg-opacity-70 backdrop-blur-sm">
+    <Portal>
+      <div className="fixed inset-0 flex items-center justify-center bg-gray-400 bg-opacity-70 backdrop-blur-sm">
       <div className="bg-white rounded-lg p-8 shadow-lg mx-4 max-w-md w-full">
         <h2 className="text-2xl font-bold mb-4 text-black">{title}</h2>
         <div className="grid grid-cols-2 gap-4 mb-4">
@@ -21,5 +23,6 @@ export default function Popup({ title, onClose, options }: PopupProps) {
         <button onClick={onClose} className="bg-red-500 text-white p-2 rounded">Zavřít</button>
       </div>
     </div>
+    </Portal>
   );
 }
